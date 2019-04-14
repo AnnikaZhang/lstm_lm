00b890a44d30b3e7501c13a75afdb10edb73eec1
AnnikaZhang <jieguotang@gmail.com>
2019-04-14 18:08:32 -0500

    change intentation

--- hw1.py
+++ hw1.py
@@ -234,12 +234,12 @@ for n_epoch in range(total_epoch):
 		x = sentence_input(sentence, t)
 		y = net(x)
 		target = get_target(sentence, t)
-                if HINGE_LOSS_FLAG:
-                        loss = hinge_loss(y, target, r, vocab_size)
-                elif BINARY_LOSS_FLAG:
-                        loss = binary_loss(y, target, r, vocab_size)
-                else:
-                        loss = criterion(y, target)
+		if HINGE_LOSS_FLAG:
+        	loss = hinge_loss(y, target, r, vocab_size)
+        elif BINARY_LOSS_FLAG:
+        	loss = binary_loss(y, target, r, vocab_size)
+        else:
+        	loss = criterion(y, target)
 		count += len(loss)
 		acc_loss += torch.sum(loss)
 		loss.sum().backward()
