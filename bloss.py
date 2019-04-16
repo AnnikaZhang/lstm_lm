b925a9eba351d2b919444b9d51a5cf8f21030ca1
AnnikaZhang <jieguotang@gmail.com>
2019-04-16 15:17:39 -0500

    123

--- bloss.py
+++ bloss.py
@@ -42,34 +42,28 @@ else:
 class myLinear(nn.Module):
 
 # a redefinition of linear function that could only output negative sample.
-	def __init__(self, in_features, out_features, bias=True):
+	def __init__(self, in_features, out_features):
 		super(myLinear, self).__init__()
 		self.in_features = in_features
 		self.out_features = out_features
 		self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
-		if bias:
-			self.bias = nn.Parameter(torch.Tensor(out_features))
-		else:
-			self.register_parameter('bias', None)
+		self.register_parameter('bias', None)
 		self.reset_parameters()
 
 	def reset_parameters(self):
 		nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-		if self.bias is not None:
-			fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
-			bound = 1 / math.sqrt(fan_in)
-			nn.init.uniform_(self.bias, -bound, bound)
+
 	def forward(self, input, sample = None):
 		print(type(sample), type(sample) == torch.Tensor)
 		if type(sample)==torch.Tensor:
 			sample.permute(1,0)
 			shrinked = torch.mm(sample, self.weight)
-			return F.linear(input, shrinked, self.bias)
-		return F.linear(input, self.weight, self.bias)
+			return F.linear(input, shrinked)
+		return F.linear(input, self.weight)
 
 	def extra_repr(self):
-		return 'in_features={}, out_features={}, bias={}'.format(
-			self.in_features, self.out_features, self.bias is not None
+		return 'in_features={}, out_features={}'.format(
+			self.in_features, self.out_features
 		)
 
 ### define the LSTM network
